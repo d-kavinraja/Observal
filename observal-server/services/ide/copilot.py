13766640de1b2c87f47c358cd6ff57b1770bf15d
Shaan Narendran <shaannaren06@gmail.com>
2026-05-24 01:35:35 +0530

    feat(optic): instrument services.ide.copilot
    Signed-off-by: Shaan Narendran <shaannaren06@gmail.com>

--- observal-server/services/ide/copilot.py
+++ observal-server/services/ide/copilot.py
@@ -5,6 +5,8 @@
 
 from __future__ import annotations
 
+from loguru import logger
+
 from schemas.ide_registry import IDE_REGISTRY
 from services.ide import ConfigContext, register_adapter
 from services.ide.helpers import (
@@ -18,9 +20,11 @@ class CopilotAdapter:
 
     @property
     def ide_name(self) -> str:
+        logger.debug("ide_name called")
         return "copilot"
 
     def format_config(self, ctx: ConfigContext) -> dict:
+        logger.debug("format_config: ctx={}", ctx)
         safe_name = ctx.safe_name
         mcp_configs = ctx.mcp_configs
         rules_content = ctx.rules_content
